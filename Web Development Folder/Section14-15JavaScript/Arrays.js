64303c1470660cbd4503d48441c6bd5fbba23f5f
seejake23 <oceansseasons@gmail.com>
2023-04-26 15:37:12 -0400

    fizzbuzz and arrays

---
+++ Web Development Folder/Section14-15JavaScript/Arrays.js
@@ -0,0 +1,12 @@
+var guestList = ["Angela", "Jack", "Pooja"];
+var potentialGuest = prompt("Please enter your name: ");
+
+if (guestList.includes(potentialGuest)) {
+    console.log("Welcome in!")
+} else {
+    console.log("You are not on the list, goodbye!")
+}
+
+
+
+//console.log(guestList[1]);
