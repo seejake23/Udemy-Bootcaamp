3ee7a7585fd2a07fbab892e9ecbe7b833fcddba4
seejake23 <oceansseasons@gmail.com>
2023-04-27 10:12:02 -0400

    fibonacci

---
+++ Web Development Folder/Section14-15JavaScript/whileLoop.js
@@ -0,0 +1,6 @@
+var numberOfBottles = 99;
+while (numberOfBottles > 0) {
+    console.log(numberOfBottles + " bottles of beer on the wall. " + numberOfBottles + " of beer.")
+    numberOfBottles--;
+    console.log("Take one down, pass it around " + numberOfBottles + " bottles of beer on the wall.")
+}
